fa77441b3341fa8f383cf732d6610c2fbabfaf64
Avishisht Gupta <avishishtgupta@gmail.com>
2024-08-27 23:31:07 +0530

    working

--- commons/src/main/java/com/geeksoftapps/whatsweb/commons/media/MediaViewPagerAdapter.kt
+++ commons/src/main/java/com/geeksoftapps/whatsweb/commons/media/MediaViewPagerAdapter.kt
@@ -1,5 +1,6 @@
 package com.geeksoftapps.whatsweb.commons.media
 
+import android.annotation.SuppressLint
 import android.content.ActivityNotFoundException
 import android.content.Context
 import android.util.SparseArray
@@ -38,8 +39,35 @@ class MediaViewPagerAdapter(
         }
     }
 
+    @SuppressLint("ClickableViewAccessibility")
     inner class ImageViewHolder(val binding: ListItemViewPagerImagePreviewBinding) :
-        RecyclerView.ViewHolder(binding.root)
+        RecyclerView.ViewHolder(binding.root) {
+        init {
+            binding.ivPreview.apply {
+                layoutParams = ConstraintLayout.LayoutParams(MATCH_PARENT, MATCH_PARENT)
+                setOnTouchListener { view, event ->
+                    var result = true
+                    //can scroll horizontally checks if there's still a part of the image
+                    //that can be scrolled until you reach the edge
+                    if (event.pointerCount >= 2 || view.canScrollHorizontally(1) && canScrollHorizontally(-1)) {
+                        //multi-touch event
+                        result = when (event.action) {
+                            MotionEvent.ACTION_DOWN, MotionEvent.ACTION_MOVE -> {
+                                parent.requestDisallowInterceptTouchEvent(true)
+                                false
+                            }
+                            MotionEvent.ACTION_UP -> {
+                                parent.requestDisallowInterceptTouchEvent(false)
+                                true
+                            }
+                            else -> true
+                        }
+                    }
+                    result
+                }
+            }
+        }
+    }
 
     inner class VideoViewHolder(val binding: ListItemViewPagerVideoPreviewBinding) :
         RecyclerView.ViewHolder(binding.root)
